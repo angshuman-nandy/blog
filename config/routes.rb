27ed6a93b3951a86e089fe74356f45054e86d6c7
Angshuman Nandy <angshuman.nandy@gmail.com>
2017-07-12 01:16:52 +0530

    init

---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'users/new'
+
+  resources :comments
+  root 'welcome#index'
+  get 'welcome/index'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
+	 resources :articles
+	 resources :users
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
